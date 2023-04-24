3acab89cd74b647acef47e772675aedf528045be
caesar <x246libra@hotmail.com>
2023-04-24 22:16:48 +0800

    refactor: channel proxy comment for default

--- channel_proxy.go
+++ channel_proxy.go
@@ -81,7 +81,7 @@ func (proxy *ChannelProxy) manager() {
 		case <-proxy.closeManager:
 			return
 
-		// default: // 高併發密集請求時, default 移除註解, 可以讓效能更好
+		// default: // default 移除註解, 可以讓效能更好
 		}
 	}
 }
